d2d2bd7f9bcfc8d9b224c5610d369405f4ebece5
Green1-j <joegr193@gmail.com>
2022-06-16 11:24:33 -0400

    redo week4 Assignment

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -4,40 +4,22 @@
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
 
+document.querySelector("#submit").addEventListener("click", () => {
+    let list = [...document.querySelectorAll("input")];
+
+    let knownLangs = list.map(input => input.value);
+    chkLang(knownLangs);
+});
 
 
 
-function chkLang(language,...langs)  {
+function chkLang(langsAry)  {
 
-//let btn = document.querySelector("#submit");
-
-//btn.addEventListener("click", chkLang);
-
-// let lang0 = document.querySelector('#lang0').value;
-// let lang1 = document.querySelector('#lang1').value;
-// let lang2 =document.querySelector('#lang2').value;
-// let lang3 =document.querySelector('#lang3').value;
-// let lang4 =document.querySelector('#lang4').value;
-// let lang5 =document.querySelector('#lang5').value;
-// let lang6 =document.querySelector('#lang6').value;
-// let lang7 =document.querySelector('#lang7').value;
-console.log(language);
-console.log(langs);
+    let lang = "JavaScript";
+    let result = langsAry.includes(lang);
 
 
-
-
-let result = langs.includes(language);
-let lang = "JavaScript";
-
-
-
-
-
-
-
-
-    // use an array method to check whether the user included 'JavaScript' in their
+ // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
 
     let obj = document.querySelector('#TestResult');
